1763a8d498b200c28354d66f72a29fb8d9e8dbf2
Vamsi-Praveen <21A91A05G5@aec.edu.in>
2024-07-25 09:20:29 +0530

    setup boiler plate and folder structure

---
+++ config/db.php
@@ -0,0 +1,16 @@
+<?php
+    $server_address = "127.0.0.1:3307";
+    $username = "root";
+    $password = "";
+    $database = "quiz";
+
+    try {
+        $conn = new mysqli($server_address,$username,$password,$database);
+        if($conn->connect_error){
+            echo "Database Connection failed";
+            exit();
+        }
+    } catch (Exception $e) {
+        echo 'Db Connection failed';
+    }
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+	$title = "Quiz";
+	include('./includes/header.php');
+?>
+
+<div class="container">
+	<h1 class="text-center text-2xl">Quiz App</h1>
+</div>
+
+
+<?php include('./includes/footer.php')?>
